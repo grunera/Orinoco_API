23fe48e9a89bbfc84a888da3447cbb2e44358058
Audrey Gruner <66530333+grunera@users.noreply.github.com>
2020-09-07 13:56:31 +0200

    first commit

---
+++ variables.js
@@ -0,0 +1,73 @@
+//let product = {
+ //_id: "5be9c8541c9d440000665243",
+ //name : "Norbert" ,
+ //price : 2900
+ //imageUrl : "http://localhost:3000/images/teddy_1.jpg"};
+
+
+//let product1 = {
+ //_id: "5beaa8bf1c9d440000a57d94",
+ //name : "Arnold" ,
+ //price : 3900
+ //imageUrl : "http://localhost:3000/images/teddy_2.jpg"};
+
+
+
+//let product2 = {
+// _id: "5beaaa8f1c9d440000a57d95",
+ //name : "Lenny and Carl" ,
+ //price : 5900
+ //imageUrl : "http://localhost:3000/images/teddy_3.jpg"};
+
+
+//let product3 = {
+ //_id: "5beaabe91c9d440000a57d96",
+ //name : "Gustav" ,
+ //price : 4500
+ //imageUrl :"http://localhost:3000/images/teddy_4.jpg",};
+
+
+//let product4 = {
+ //_id: "5beaacd41c9d440000a57d97",
+ //name : "Garfunkel" ,
+ //price : 5500
+ //imageUrl : "http://localhost:3000/images/teddy_5.jpg"};
+
+
+//let products = [];
+//products.push(product1,product2,product3,product4,product5);
+let product = {
+ _id: "5be9c8541c9d440000665243",
+ name : "Norbert" ,
+ price : 2900
+ imageUrl : "http://localhost:3000/images/teddy_1.jpg"};
+
+
+let product1 = {
+ _id: "5beaa8bf1c9d440000a57d94",
+ name : "Arnold" ,
+ price : 3900
+ imageUrl : "http://localhost:3000/images/teddy_2.jpg"};
+
+
+
+
+let product2 = {
+ _id: "5beaaa8f1c9d440000a57d95",
+ name : "Lenny and Carl" ,
+ price : 5900
+ imageUrl : "http://localhost:3000/images/teddy_3.jpg"};
+
+
+let product3 = {
+ _id: "5beaabe91c9d440000a57d96",
+ name : "Gustav" ,
+ price : 4500
+ imageUrl :"http://localhost:3000/images/teddy_4.jpg"};
+
+
+let product4 = {
+ _id: "5beaacd41c9d440000a57d97",
+ name : "Garfunkel" ,
+ price : 5500
+ imageUrl : "http://localhost:3000/images/teddy_5.jpg"};
